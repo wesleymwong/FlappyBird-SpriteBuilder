f286d6f023b80aa92e6d55c23c9d1adf081aa5ae
Wesley Wong <wesleywong@Wesleys-MacBook-Pro.local>
2014-11-01 17:29:36 -0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -11,6 +11,7 @@
     character = (Character*)[CCBReader load:@"Character"];
     //method: [ObjectName: methodName:parameter]
     [physicsNode addChild:character];
+    [self addObstacle]; 
 }
 
 -(void)update:(CCTime)delta
@@ -21,7 +22,7 @@
 // put new methods here
 - (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
     // this will get called every time the player touches the screen
-    [character flap]; 
+    [character flap];
 }
 
 @end
